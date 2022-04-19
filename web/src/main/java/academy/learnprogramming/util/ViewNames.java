8305ee566740538760e4479b6d375cbbb37f5524
Vincent Soweto <vincentsoweto22@gmail.com>
2022-04-19 11:59:42 +0200

    fully functional guess the number game

---
+++ web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String HOME = "home";
+    public static final String PLAY = "play";
+    public static final String GAME_OVER = "game-over";
+    // == constructor ==
+    private ViewNames(){
+
+    }
+}
